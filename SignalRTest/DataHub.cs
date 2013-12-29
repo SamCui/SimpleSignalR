f53b106d5d8a59ad695443954ea73bba088bdd85
SamCui <caimu.cui@gmail.com>
2013-12-29 06:20:50 -0500

    other files are included this time

---
+++ SignalRTest/DataHub.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace SignalRTest
+{
+    public class DataHub: Hub
+    {
+        //public DataHub()
+        //{
+        //}
+        public void Connect(string userName)
+        {
+            var id = Context.ConnectionId;
+            Clients.Caller.onConnected(id);
+            SendMessage(userName);
+            if(userName =="sam")
+                SendPrivateMessage(userName);
+        }
+
+        public void SendMessage(string userName)
+        {
+            Clients.All.messageReceived(userName + " " +"says", "there is a message from the server");
+        }
+
+        public void SendPrivateMessage(string userName)
+        {
+            var id = Context.ConnectionId;
+            Clients.Client(id).sendPrivateMessage(id, userName,  "received a private message");
+            Clients.Caller.sendPrivateMessage(id, userName, "received a private message 2"); 
+        }
+    }
+}
